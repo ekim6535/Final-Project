8ef3bc02b27a204c9f2528ba859a7c39f52d9bf8
ekim6535 <144280248+ekim6535@users.noreply.github.com>
2024-12-13 14:44:59 -0500

    Update data_analysis.rs
    Adjusted the calculate_statistics function to match the new housing dataset I am using

--- data_analysis.rs
+++ data_analysis.rs
@@ -5,12 +5,12 @@ pub mod data_analysis {
         let total_houses = data.len() as f64;
 
         let avg_price = data.iter().map(|r| r.price).sum::<f64>() / total_houses;
-        let avg_bedrooms = data.iter().map(|r| r.bedrooms).sum::<u32>() as f64 / total_houses;
-        let avg_bathrooms = data.iter().map(|r| r.bathrooms).sum::<u32>() as f64 / total_houses;
+        let avg_bedrooms = data.iter().map(|r| r.bedroom_count).sum::<u32>() as f64 / total_houses;
+        let avg_age = data.iter().map(|r| r.age).sum::<u32>() as f64 / total_houses;
 
         HashMap::from([
             ("average_price", avg_price),
             ("average_bedrooms", avg_bedrooms),
-            ("average_bathrooms", avg_bathrooms),
+            ("average_age", avg_age),
         ])
     }
