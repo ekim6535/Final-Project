58141b7beb0b01cb744cc8d1c466d2a76adabffd
ekim6535 <144280248+ekim6535@users.noreply.github.com>
2024-12-13 17:15:39 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ mod data_analysis;
 mod visualization;
 mod tests;
 
-use data_loader::load_data;
-use data_analysis::{calculate_statistics, find_price_trends, correlation_analysis};
-use visualization::generate_histogram;
+use data_loader::data_loader::load_data;
+use data_analysis::data_analysis::{calculate_statistics, find_price_trends, correlation_analysis};
+use visualization::visualization::generate_histogram;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
