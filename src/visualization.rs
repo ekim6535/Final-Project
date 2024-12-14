d42cadde5fc50da06d52804cdb0452037051ca6e
ekim6535 <144280248+ekim6535@users.noreply.github.com>
2024-12-13 19:55:25 -0500

    Update visualization.rs
    Debugging

--- src/visualization.rs
+++ src/visualization.rs
@@ -1,41 +1,39 @@
-pub mod visualization {
-    use crate::data_loader::data_loader::HouseRecord;
-    use plotters::prelude::*;
-    use std::error::Error;
-    
-    pub fn generate_histogram<F>(
-        data: &[HouseRecord],
-        field_name: &str,
-        field_extractor: F,
-        output_path: &str,
-    ) -> Result<(), Box<dyn Error>>
-    where
-        F: Fn(&HouseRecord) -> f64,
-    {
-        let values: Vec<f64> = data.iter().map(&field_extractor).collect();
+use crate::data_loader::HouseRecord;
+use plotters::prelude::*;
+use std::error::Error;
 
-        let root = BitMapBackend::new(output_path, (640, 480)).into_drawing_area();
-        root.fill(&WHITE)?;
+pub fn generate_histogram<F>(
+    data: &[HouseRecord],
+    field_name: &str,
+    field_extractor: F,
+    output_path: &str,
+) -> Result<(), Box<dyn Error>>
+where
+    F: Fn(&HouseRecord) -> f64,
+{
+    let values: Vec<f64> = data.iter().map(&field_extractor).collect();
 
-        let mut chart = ChartBuilder::on(&root)
-            .caption(format!("Histogram of {}", field_name), ("sans-serif", 30))
-            .margin(10)
-            .x_label_area_size(30)
-            .y_label_area_size(30)
-            .build_cartesian_2d(
-                *values.iter().min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap()..
-                *values.iter().max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap(),
-                0..values.len() / 10,
-            )?;
+    let root = BitMapBackend::new(output_path, (640, 480)).into_drawing_area();
+    root.fill(&WHITE)?;
 
-        chart.configure_mesh().draw()?;
-
-        chart.draw_series(
-            Histogram::vertical(&chart)
-                .style(RED.mix(0.5).filled())
-                .data(values.iter().map(|&v| (v, 1))),
+    let mut chart = ChartBuilder::on(&root)
+        .caption(format!("Histogram of {}", field_name), ("sans-serif", 30))
+        .margin(10)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(
+            *values.iter().min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap()
+                ..*values.iter().max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap(),
+            0..values.len() / 10,
         )?;
 
-        Ok(())
-    }
+    chart.configure_mesh().draw()?;
+
+    chart.draw_series(
+        Histogram::vertical(&chart)
+            .style(RED.mix(0.5).filled())
+            .data(values.iter().map(|&v| (v, 1))),
+    )?;
+
+    Ok(())
 }
